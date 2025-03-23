0115b5a429ee47c0ec90b02663d1bd2414620538
abdelrahman.elgharib@ext.mahle.com <abdelrahman.elgharib@ext.mahle.com>
2025-03-23 12:22:59 +0200

    initial pressure sensor with esp

--- Server.c
+++ Server.c
@@ -8,6 +8,7 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include <Adafruit_BMP280.h>
  
 // Initialize all pointers
 BLEServer* pServer = NULL;                        // Pointer to the server
@@ -43,8 +44,28 @@ class MyServerCallbacks: public BLEServerCallbacks {
 };
  
 void setup() {
-  Serial.begin(115200);
- 
+  Serial.begin(9600);
+   unsigned status;
+  //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+ status = bmp.begin(0x76);
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+  }
+
+   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+
   // Create the BLE Device
   BLEDevice::init("ESP32");
  
@@ -95,6 +116,16 @@ void setup() {
 }
  
 void loop() {
+
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+ 
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+
     // notify changed value
     if (deviceConnected) {
       // pCharacteristic_1 is an integer that is increased with every second
